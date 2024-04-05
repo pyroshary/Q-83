9e9932dbfeb7097ae9fdae0c848fce123aab78fa
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-06 04:05:14 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function convertCase(str) {
+    var upperCase = str.toUpperCase();
+    var lowerCase = str.toLowerCase();
+    console.log("Uppercase:", upperCase, "Lowercase:", lowerCase);
+}
+;
+convertCase("HEY SHARY");
---
+++ index.ts
@@ -0,0 +1,8 @@
+ function convertCase (str:string){
+    let  upperCase = str.toUpperCase();
+    let  lowerCase = str.toLowerCase()
+
+    console.log("Uppercase:",upperCase,"Lowercase:",lowerCase);
+ };
+
+convertCase("HEY SHARY")
